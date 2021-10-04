225d168f45c45e2e87ff0ac4aa0fe393910fba25
André Martins <andre.martins@uds.com.br>
2021-10-03 22:24:42 -0300

    Criado componente ErrorPage

---
+++ src/views/template/pages/ErrorPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-container
+    class="vh-100 d-flex flex-column align-items-center justify-content-center"
+  >
+    <span style="font-size: 80px">&times;</span>
+    <h1>
+      Ops, {{ forbidden ? "você não pode continuar" : "página não encontrada" }}
+    </h1>
+    <p>
+        {{ forbidden ? "Sem permissão para acessar a página solicitada"
+        : "Verifique o endereço digitado e tente novamente" }}
+    </p>
+    <b-btn
+      class="mt-5"
+      @click="$router.push({ name: $auth.check() ? 'dashboard' : 'auth' })"
+    >
+      <b-icon icon="arrow-left" />
+      Voltar
+    </b-btn>
+  </b-container>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'ErrorPage',
+  computed: {
+    forbidden() {
+      return !!this.$route.path.match(/403/)?.length;
+    },
+  },
+});
+</script>
